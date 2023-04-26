a34e1f706a556fd3c5aeba23224b9c0ec6b5a634
howardbaek <howardbaek.fh@gmail.com>
2023-04-26 15:53:18 -0700

    Update fh_colors

--- R/hutch_colors.R
+++ R/hutch_colors.R
@@ -12,11 +12,12 @@
 #' @export
 hutch_colors <- function(...) {
   fh_colors <- c("Dark Navy" = "#1B365D", 
-                 "Bright Blue" = "#00ABC8", 
+                 "Bright Blue" = "#00C1D5", 
                  "Warm Purple" = "#AA4AC4", 
                  "Vivid Yellow" = "#FFB500", 
                  "Teal" = "#0A799A", 
-                 "Light Gray" = "#F4F4F4")
+                 "Light Gray" = "#F4F4F4",
+                 "White" = "#FFFFFF")
   colors_ <- c(...)
   
   if (is.null(colors_)) {
